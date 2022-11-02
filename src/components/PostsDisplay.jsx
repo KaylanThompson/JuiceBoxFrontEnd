b58541f55afca912b32aeb2cbad7e8fb92849ce2
Kaylan Thompson <kaylanthompson@icloud.com>
2022-11-02 18:44:56 -0500

    post components working properly

--- src/components/PostsDisplay.jsx
+++ src/components/PostsDisplay.jsx
@@ -5,30 +5,13 @@ import SinglePost from "./SinglePost"
 
 const PostsDisplay = ({postList}) => {
 
-
-
-
     return (
         <div>
-            {postList.map((post) => {
+            {postList.map(post => {
                 return (
-                    <SinglePost postList={postList} key={post.id} />
+                    <SinglePost postList={postList} key={post.id} post={post}/>
                 )})}
         </div>
-        // <div> {postList ? 
-        //     postList.map((post) => {
-        //         return (
-        //             <div className="single-post"
-        //             key={post.id}>
-        //             <h3>{post.title}</h3>
-        //             <p><b>Author: </b>{post.author.username}</p>
-        //             <p>{post.content}</p>
-        //             {post.tags.map(tag => {
-        //                 return (
-        //                 <div key={tag.id}><p>{tag.name}</p></div>)})}
-        //     </div>     
-        //         )
-        //     })
 
 
     )
--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const SinglePost = ({postList}) => {
+const SinglePost = ({post}) => {
     
     return (
         <div>
 
         <div className="single-post" key={post.id}>
-            
+
         <h3>{post.title}</h3>
         <p><b>Author: </b>{post.author.username}</p>
         <p>{post.content}</p>
