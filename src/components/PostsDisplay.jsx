ada156a20f5c2bd2bac19c54dbcd7428fdb3c3fe
Kaylan Thompson <kaylanthompson@icloud.com>
2022-11-02 14:28:51 -0500

    still have an error that needs resolving on post

--- src/components/PostsDisplay.jsx
+++ src/components/PostsDisplay.jsx
@@ -1,30 +1,36 @@
 import React from "react"
+import SinglePost from "./SinglePost"
 
 
 
 const PostsDisplay = ({postList}) => {
-    console.log(postList)
+
+
 
 
     return (
-        <div> {postList ? 
-            postList.map((post) => {
+        <div>
+            {postList.map((post) => {
                 return (
-                    <div className="single-post"
-                    key={post.id}>
-                    <h3>{post.title}</h3>
-                    <p><b>Author: </b>{post.author.username}</p>
-                    <p>{post.content}</p>
-                    {post.tags.map(tag => {
-                        return (
-                        <div key={tag.id}><p>{tag.title}</p></div>)})}
-        
-
-            </div>     
-                )
-            })
-        : console.log("nothing is being returned")}
+                    <SinglePost postList={postList} key={post.id} />
+                )})}
         </div>
+        // <div> {postList ? 
+        //     postList.map((post) => {
+        //         return (
+        //             <div className="single-post"
+        //             key={post.id}>
+        //             <h3>{post.title}</h3>
+        //             <p><b>Author: </b>{post.author.username}</p>
+        //             <p>{post.content}</p>
+        //             {post.tags.map(tag => {
+        //                 return (
+        //                 <div key={tag.id}><p>{tag.name}</p></div>)})}
+        //     </div>     
+        //         )
+        //     })
+
+
     )
 }
 
--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 
-
-
-
-const SinglePost = () => {
+const SinglePost = ({postList}) => {
     
     return (
-        <div className="single-post"
-                    key={post.id}>
-                    <h3>{post.title}</h3>
-                    <p><b>Author: </b>{post.author.username}</p>
-                    <p>{post.content}</p>
-                    {post.tags.map(tag => {
-                        return (
-                        <div className="post-tags" key={tag.id}><p>{tag.name}</p></div>)})}
-        </div>    
-    
+        <div>
+
+        <div className="single-post" key={post.id}>
+            
+        <h3>{post.title}</h3>
+        <p><b>Author: </b>{post.author.username}</p>
+        <p>{post.content}</p>
+        {post.tags.map(tag => {
+            return (
+                <div key={tag.id}><p>{tag.name}</p></div>)})}
+        </div> 
+ 
+            </div>
+
     )
 }
 
